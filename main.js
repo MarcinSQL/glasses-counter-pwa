7f4cbf0dabe4bd1daea6ef6684ff29ed97d46582
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-08-21 21:37:18 +0200

    Added working counter

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-const counter = document.querySelector('#main__glass__counter--js');
+const counterField = document.querySelector('#main__glass__counter--js');
 const addBtn = document.querySelector('#add-btn--js');
 const removeBtn = document.querySelector('#remove-btn--js');
+
+let counter = Number(counterField.textContent);
+
+addBtn.addEventListener('click', () => {
+    counter += 1;
+    counterField.textContent = counter;
+})
+
+removeBtn.addEventListener('click', () => {
+    if(counter > 0){
+        counter = counter - 1;
+        counterField.textContent = counter;
+    } 
+})
