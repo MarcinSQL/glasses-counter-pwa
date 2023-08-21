5d860c58a10c3495bb49d32add67dd26c0a440cd
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-08-21 22:27:02 +0200

    Added simple animation to add button

--- main.js
+++ main.js
@@ -1,28 +1,43 @@
-const counterField = document.querySelector('#main__glass__counter--js');
-const addBtn = document.querySelector('#add-btn--js');
-const removeBtn = document.querySelector('#remove-btn--js');
+const counterField = document.querySelector("#main__glass__counter--js");
+const addBtn = document.querySelector("#add-btn--js");
+const removeBtn = document.querySelector("#remove-btn--js");
 const key = new Date().toISOString().slice(0, 10);
 
-if(localStorage.getItem(key) == null){
-    localStorage.setItem(key, 0);
+if (localStorage.getItem(key) == null) {
+  localStorage.setItem(key, 0);
 }
 
 counterField.textContent = localStorage.getItem(key);
 
 let counter = Number(counterField.textContent);
 
-console.log(localStorage.getItem(key));
+addBtn.addEventListener("click", () => {
+  counter += 1;
+  counterField.textContent = counter;
+  localStorage.setItem(key, counter);
+});
 
-addBtn.addEventListener('click', () => {
-    counter += 1;
+removeBtn.addEventListener("click", () => {
+  if (counter > 0) {
+    counter = counter - 1;
     counterField.textContent = counter;
     localStorage.setItem(key, counter);
-})
+  }
+});
 
-removeBtn.addEventListener('click', () => {
-    if(counter > 0){
-        counter = counter - 1;
-        counterField.textContent = counter;
-        localStorage.setItem(key, counter);
-    } 
-})
+var animateButton = (e) => {
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove("animate");
+
+  e.target.classList.add("animate");
+  setTimeout(function () {
+    e.target.classList.remove("animate");
+  }, 700);
+};
+
+var bubblyButtons = document.getElementsByClassName("main__add-btn");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener("click", animateButton, false);
+}
