edcc7e47428093949113bd1d486908aa60ee5cfe
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-08-21 21:19:10 +0200

    Added querySelectors

---
+++ main.js
@@ -0,0 +1,3 @@
+const counter = document.querySelector('#main__glass__counter--js');
+const addBtn = document.querySelector('#add-btn--js');
+const removeBtn = document.querySelector('#remove-btn--js');
